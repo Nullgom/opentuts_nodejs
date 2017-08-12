c2b73a4482fb12d36ecdf2f269294c29fd0d54b5
Whoisy Wow <surfwon@gmail.com>
2017-08-12 10:46:49 +0000

    라우팅과 정적폴더 설정

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
 	res.send(`
 	<!DOCTYPE html>
 	<html>
-	<head><title>Express Web</title></head>
+	<hd><title>Express Web</title></head>
 	<body>
 	<h1>Welcome to Express.</h1>
 	<h1>Hello World!</h1>
 	</body>
 	</html>`);
 });
+app.get('/route', function(req, res){
+	res.send('Hello Router, <img src="/images/textcloud.jpg" />');	
+});
+
 app.get('/login', function(req, res){
 	res.send('<h1>Login Please!</h1>');
 });
