496c8feae6079a1fd0f7f2d1f91036a0bbe03eb4
Whoisy Wow <surfwon@gmail.com>
2017-08-12 13:38:10 +0000

    Pug 테플릿 엔진 적용

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.locals.pretty = true; // html 소스 표시를 계층구조로 표시하도록 합니다.
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-	res.send(`
-	<!DOCTYPE html>
-	<html>
-	<hd><title>Express Web</title></head>
-	<body>
-	<h1>Welcome to Express.</h1>
-	<h1>Hello World!</h1>
-	</body>
-	</html>`);
+app.get('/template', function(req, res) {
+	res.render('temp', {
+		'title': 'Pug(Jade)',
+		'time':Date()
+	});
 });
+
+app.get('/', function(req, res){
+	res.send('Hello, Home Page!');
+});
+
 app.get('/dynamic', function(req, res){
 	var lis = '';
 	for(var i=0; i < 5; i++) {
