62f0333106450895175063c8be574d922eb9e145
Whoisy Wow <surfwon@gmail.com>
2017-08-14 05:40:30 +0000

    Session 사용하기

---
+++ app_session.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'TheQuickBrownFoxJumpsOverLazyDog',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}));
+
+// 세션을 이용한 카운터
+app.get('/count', function (req, res) {
+	if(req.session.count) {
+		req.session.count ++;	
+	} else {
+		req.session.count = 1;	
+	}
+	
+	res.send('Count : ' + req.session.count);
+});
+
+// 세션을 이용한 로그인
+
+app.get('/welcome', function(req, res) {
+	if(req.session.displayName) {
+		res.send(`
+			<h1>Hello, ${req.session.displayName}</h1>
+			<a href="/auth/logout">Logout</a>
+		`);		
+	} else {
+		res.send(`
+			<h1>Welcome !!</h1>
+			<a href="/auth/login">Login</a>
+		`);
+	}
+});
+
+app.get('/auth/logout', function(req, res) {
+	delete req.session.displayName;
+	res.redirect('/welcome');
+});
+
+app.post('/auth/login', function(req, res) {
+	var user = {
+		username: 'egoing',
+		password: '112233',
+		displayName: 'Egoing'
+	};
+	var uname = req.body.username,
+		pwd = req.body.password;
+	
+	if(uname === user.username && pwd === user.password) {
+		req.session.displayName = user.displayName;
+		res.redirect('/welcome');
+	} else {
+		res.send('Who are you? <a href="/auth/login">Login</a>');
+	}
+});
+
+app.get('/auth/login', function(req, res) {
+	var output = `
+	<h1>LOGIN</h1>
+	<form action="/auth/login" method="POST">
+		<p><input type="text" name="username" placeholder="사용자이름 입력" /></p>
+		<p><input type="password" name="password" placeholder="비밀번호 입력" /></p>
+		<p><input type="submit" value="로그인"/></p>
+	</form>
+	`;
+	if(!req.session.displayName) {
+		res.send(output);	
+	} else {
+		res.redirect('/welcome');
+	}
+	
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
